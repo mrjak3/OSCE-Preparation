9fc3d3ddfcfec57ef358e359705f63d5de9b64e7
jacobforer <jacobforer@JACFOR-COTP-1.fios-router.home>
2016-10-17 10:03:23 -0400

    more python

---
+++ Pentesting with Python/Module 3/Client_tcp_socket_server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+tcpSocket.connect((sys.argv[1], 8000))
+
+while 1 :
+	userInput = raw_input("Please enter a string: ")
+	tcpSocket.send(userInput)
+	print tcpSocket.recv(2048)
+
+tcpSocket.close()
